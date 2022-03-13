1ed988058c797c04715ac860e2dedabba3dc7158
Carson Rottinghaus <cqrottinghaus@gmail.com>
2022-03-12 19:28:48 -0600

    Added saving functions and confirmation dialogs

--- text-editor/MainWindow.xaml.cs
+++ text-editor/MainWindow.xaml.cs
@@ -17,29 +17,52 @@ using System.IO;
 
 namespace text_editor
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    /// Carson Rottinghaus
     public partial class MainWindow : Window
     {
-        bool canSave, canSaveAs = false;
+        bool textChanged;
         TextDocument td = new TextDocument();
         public MainWindow()
         {
             InitializeComponent();
+            fastSaveBtn.IsEnabled = false;
+        }
+
+        private void textChangedEventHandler(object sender, TextChangedEventArgs args)
+        {
+            textChanged = true;
+            fastSaveBtn.IsEnabled = true;
         }
 
         public void New_File(Object sender, System.EventArgs e)
         {
+            if (textChanged)
+            {
+                int result = prompt_unsaved_changes();
+                if(result == -1)
+                {
+                    return;
+                }
+            }
             td = new TextDocument();
             textEditorBox.Text = td.Content;
+            Title = "New Document";
+            fastSaveBtn.IsEnabled = false;
+            textChanged = false;
         }
 
         public void Open_File(Object sender, System.EventArgs e)
         {
+            if (textChanged)
+            {
+                int result = prompt_unsaved_changes();
+                if (result == -1)
+                {
+                    return;
+                }
+            }
             OpenFileDialog ofd = new OpenFileDialog();
-            ofd.InitialDirectory = "c:\\";
-            ofd.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            ofd.Filter = "txt files (*.txt)|*.txt";
             ofd.FilterIndex = 2;
             ofd.RestoreDirectory = true;
 
@@ -47,37 +70,100 @@ namespace text_editor
             {
                 td.Open(ofd); //Sets TextDocument Class Variables
                 textEditorBox.Text = td.Content;
-                this.Title = td.filePath;
-                canSave = true;
-                canSaveAs = true;
+                Title = td.fileName;
+                textChanged = false;
+                fastSaveBtn.IsEnabled = false;
             }
-        }
 
-        public void Save_File(Object sender, System.EventArgs e)
-        {
-            if (canSave)
-            {
-
-            }
-        }
-
-        public void Save_File_As(Object sender, System.EventArgs e)
-        {
-            if (canSaveAs)
-            {
-                SaveFileDialog sfd = new SaveFileDialog();
-                sfd.InitialDirectory = "c:\\";
-                sfd.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
-                sfd.ShowDialog();
-                canSave = true;
-            }
         }
 
         public void Exit(object sender, System.EventArgs e)
         {
+            if (textChanged)
+            {
+                int result = prompt_unsaved_changes();
+                if (result == -1)
+                {
+                    return;
+                }
+                textChanged = false;
+            }
             System.Windows.Application.Current.Shutdown();
         }
 
+        public void Fast_Save_File(Object sender, System.EventArgs e)
+        {
+            if (textChanged)
+            {
+                td.Content = textEditorBox.Text;
+                if (td.filePath == "") //If file is new
+                {
+                    Save_File_Dialog(); //Prompt for save location
+                }
+                else
+                {
+                    td.Save();
+                    fastSaveBtn.IsEnabled = false;
+                }
+                textChanged = false;
+            }
+        }
 
+        public void Save_Location_Handler(Object sender, System.EventArgs e)
+        {
+            Save_File_Dialog();
+        }
+
+        public void Open_About(object sender, System.EventArgs e)
+        {
+            string message = "Created by Carson Rottinghaus\n\nThe text editor has the following functions:\nNew: Creates a new blank text document\nOpen: Opens a prompt to select a text document" +
+                "\nSave: Saves a text document, prompts user with a file location if one is not saved\nSave As: Prompts user with a save location to save the file\nExit: Exits the program";
+            string caption = "About";
+            System.Windows.Forms.MessageBox.Show(message, caption);
+        }
+        private void Save_File_Dialog()
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "txt files (*.txt)|*.txt";
+            sfd.RestoreDirectory = true;
+            if (sfd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                td.Content = textEditorBox.Text;
+                td.Save(sfd);
+                Title = td.fileName;
+                textChanged = false;
+                fastSaveBtn.IsEnabled = false;
+            }
+        }
+        private int prompt_unsaved_changes()
+        {
+            string message = "Your current document has not been saved. Do you want to save your changes?";
+            string caption = "Unsaved Changes";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNoCancel;
+            DialogResult result;
+            result = System.Windows.Forms.MessageBox.Show(message, caption, buttons);
+            switch (result)
+            {
+                case System.Windows.Forms.DialogResult.Cancel:
+                    return -1;
+
+                case System.Windows.Forms.DialogResult.Yes:
+                    if (td.filePath == "")
+                    {
+                        Save_File_Dialog();
+                    }
+                    else
+                    {
+                        td.Content = textEditorBox.Text;
+                        td.Save();
+                    }
+                    fastSaveBtn.IsEnabled = false;
+                    return 0;
+
+                case System.Windows.Forms.DialogResult.No:
+                    return 0;
+            }
+            return 0;
+        }
     }
 }
--- text-editor/TextDocument.cs
+++ text-editor/TextDocument.cs
@@ -12,6 +12,7 @@ namespace text_editor
     {
         //Auto implemented properties
         public string filePath { get; set; }
+        public string fileName { get; set; }
         public string Content { get; set; }
 
         public TextDocument()
@@ -24,6 +25,7 @@ namespace text_editor
         {
             var fileStream = ofd.OpenFile();
             filePath = ofd.FileName;
+            fileName = Path.GetFileName(filePath);
             StreamReader reader = new StreamReader(fileStream);
             var fileContent = reader.ReadToEnd();
             Content = fileContent;
@@ -32,19 +34,18 @@ namespace text_editor
         public void Save(SaveFileDialog sfd)
         {
             filePath = sfd.FileName;
-
+            fileName = Path.GetFileName(filePath);
+            Save();
         }
 
 
         public void Save()
         {
-            
+            FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.Write(Content);
+            sw.Close();
+            fs.Close();
         }
-        //public string Parse_File_Path(string fileName)
-        //{
-            //List<String> fileNameArray = fileName.Split("//").ToList;
-            //return fileNameArray;
-        //}
-                    
     }
 }
